d20cfb744a0e666e98412afada78718c8fe78432
Heraldo Espozel <hespozel@gmail.com>
2017-10-11 15:56:53 -0300

    Initial commit

---
+++ SVD 3.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 12:01:34 2017
+
+@author: hespo
+"""
+
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+
+from PIL import Image
+import os
+os.chdir("C:/Users/hespo/OneDrive/Documentos/GitHub/Trab2_DataScience/gs")
+cwd = os.getcwd()
+print (cwd)
+img = Image.open('1.jpg')
+imggray = img.convert('LA')
+plt.figure(figsize=(9, 6))
+plt.imshow(imggray);
+
+imgmat = np.array(list(imggray.getdata(band=0)), float)
+imgmat.shape = (imggray.size[1], imggray.size[0])
+imgmat = np.matrix(imgmat)
+plt.figure(figsize=(9,6))
+plt.imshow(imgmat, cmap='gray');
+
+U, sigma, V = np.linalg.svd(imgmat)
+
+reconstimg = np.matrix(U[:, :1]) * np.diag(sigma[:1]) * np.matrix(V[:1, :])
+plt.imshow(reconstimg, cmap='gray');
+
+for i in range(2, 4):
+    reconstimg = np.matrix(U[:, :i]) * np.diag(sigma[:i]) * np.matrix(V[:i, :])
+    plt.imshow(reconstimg, cmap='gray')
+    title = "n = %s" % i
+    plt.title(title)
+    plt.show()
+
+for i in range(5, 51, 5):
+    reconstimg = np.matrix(U[:, :i]) * np.diag(sigma[:i]) * np.matrix(V[:i, :])
+    plt.imshow(reconstimg, cmap='gray')
+    title = "n = %s" % i
+    plt.title(title)
+    plt.show()
+    
+print ("FIM")
